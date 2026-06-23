41ea71ffa00d7d98bd859862acacc81410f5603a
Vish Sali <saliv4402@gmail.com>
2026-06-23 11:16:16 +0530

    Time: 4 ms (40.57%), Space: 20.4 MB (59.36%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -12,3 +12,7 @@ class Solution:
             map[nums[i]] = i
             
         return [-1, -1]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
